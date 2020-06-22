225df4f1dbc47348b0c338d187e1659b41a9d86f
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2020-06-22 17:06:43 +0530

    Adding Placeholder APIs

--- beingzero/authentication-service-express-node-mongo/server.js
+++ beingzero/authentication-service-express-node-mongo/server.js
@@ -6,10 +6,47 @@ const dbconnect = require('./backend/db/mongoconnect');
 
 const port = config.web_port
 
-dbconnect.connect(true);
+//dbconnect.connect(true);
 
 app.use(morgan('dev'))
-app.get('/', (req, res) => res.send( 'Welcome to '+config.service_name+' '+config.service_version));
+app.use(express.json())
 
+function logRequest(req){
+    console.log("HEADERS: " + JSON.stringify(req.headers));
+    console.log("PARAMS: " + JSON.stringify(req.params));
+    console.log("QUERY: " + JSON.stringify(req.query));
+    console.log("BODY: " + JSON.stringify(req.body));
+}
+
+app.get('/', function(req, res){
+    logRequest(req);
+    res.send( 'Welcome to '+config.service_name+' '+config.service_version);
+})
+
+app.post('/api/v1/register', function(req, res){
+    logRequest(req);
+    res.json({
+        username: '',
+        token: '',
+        message: ''
+    });
+})
+app.post('/api/v1/login', function(req, res){
+    logRequest(req);
+    res.json({
+        username: '',
+        token: '',
+        message: ''
+    });
+})
+
+
+app.get('/api/v1/greeting/public', function(req, res){
+    res.json({ greeting: "Welcome to API"});
+})
+
+app.get('/api/v1/greeting/protected', function(req, res){
+    res.json({  message: "Welcome to Authenticated API" });
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
